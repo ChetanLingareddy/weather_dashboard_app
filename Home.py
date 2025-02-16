4fe329a09090fca5beeaa3ff63cd336eba6967c8
ChetanLingareddy <lingareddychetan@gmail.com>
2025-02-16 01:39:16 -0600

    Final code with documentation and changes

--- Home.py
+++ Home.py
@@ -1,14 +1,56 @@
 import streamlit as st
 import plotly.express as px
+
 from backend import get_data
 
-st.title("Weather Forecast for the Next Days")
+st.title("Weather Forecast for the Next 5 Days")
 
+# Frontend , take values from user
 place=st.text_input("Place: ")
 days=st.slider(label="Forcast Days:", min_value=1,max_value=5, help="Select number of forecasted days")
 option = st.selectbox( "Select data to view", ("Temperature", 'Sky') )
-st.subheader(f"{option} for next {days} days in {place}")
 
-get_data( place , days , option)
-figure= px.line(x=d, y=t, labels={"x": "date", "y": "temperature (c)"})
-st.plotly_chart(figure)
+# try error block for error handling
+try:
+    if place:
+        st.subheader ( f"{option} for next {days} days in {place}" )
+
+        #Getting data from backend
+        filtered_data = get_data ( place, days )
+
+        if option == "Temperature":
+
+            #getting  temperature and dates from filtered_data
+            temperature = [value["main"]["temp"]/10 for value in filtered_data]
+            dates = [value["dt_txt"] for value in filtered_data]
+
+            # Printing a graph with the data using plotly
+            figure= px.line(x=dates, y=temperature, labels={"x": "date", "y": "temperature (c)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "rain":"images/rain.png", "Snow":"images/snow.png"}
+
+            #getting  sky_condition  from filtered_data
+            sky_conditions = [value["weather"][0]["main"] for value in filtered_data]
+
+            #image_path = [images[condition] for condition in sky_conditions]
+            #st.image(image_path, width=115)
+            max_cols = 6
+            day_count = 1  # Start day count
+
+            # Loop through sky_conditions with step-size of 8
+            for i in range ( 0, len ( sky_conditions ), 8 ) :
+                st.subheader ( f"Day {day_count}" )  # Print day count
+
+                # Create columns for up to 6 images in a row
+                cols = st.columns ( max_cols )
+
+                # Get the next 8 images, but only display 6 per row
+                for j, condition in enumerate ( sky_conditions[i :i + 8] ) :
+                    with cols[j % max_cols] :  # Use modulo to ensure 6 images per row
+                        st.image ( images[condition], width=115, caption=condition )
+
+                day_count += 1
+except KeyError: st.error("Error: City Doesn't Exist,  Try with a correct name.")
--- backend.py
+++ backend.py
@@ -1,22 +1,21 @@
 import os
 import requests
 
+# Stored api key locally
 api_key = os.getenv("Weather_app_API_key")
 
-def get_data(place ,forecast_days, kind):
+def get_data(place ,forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
+    # Gets data and stores it in JSON format
     request = requests.get(url)
     data = request.json()
+    # Since data gets updated every 3 hours, we have 8 data points for each day
     filtered_data = data['list']
     filtered_data = filtered_data[:8 * forecast_days]
-    if kind == "Temperature":
-        filtered_data = [value["main"]["temp"] for value in filtered_data]
-    if kind == "Sky":
-        filtered_data = [value["weather"][0]["main"] for value in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
-    get_data(place="kansas", forecast_days=3, kind="Sky")
+    get_data(place="kansas", forecast_days=3)
 
 
 
